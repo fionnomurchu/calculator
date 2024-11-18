24eb0b7775e478c894759e60466ae67aff42eb9c
eoinokelly <eoinokelly90@gmail.com>
2024-11-18 15:57:52 +0000

    Changes char array to a list array Iterates through list concatenating the numbers Appends the concatenated numbers and the symbols to final list

---
+++ src/CalcMain.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalcMain {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter number");
+        String a = input.nextLine();
+        System.out.println(a);
+        String[] stringArray = a.split("");
+
+        List<String> finalList = new ArrayList<>();
+        String value = "";
+        String previous = "";
+
+        for (String s : stringArray) {
+            if (s.equals("0") || s.equals("1") || s.equals("2")
+                    || s.equals("3") || s.equals("4")
+                    || s.equals("5") || s.equals("6")
+                    || s.equals("7") || s.equals("8")
+                    || s.equals("9")
+            ) {
+                value += s;
+                previous = s;
+            }
+            else if (s.equals("+")) {
+                finalList.add(value);
+                finalList.add("+");
+                value = "";
+            }
+            else if (s.equals("-")) {
+                finalList.add(value);
+                finalList.add("-");
+                value = "";
+            }
+            else if (s.equals("*")) {
+                finalList.add(value);
+                finalList.add("*");
+                value = "";
+            }
+            else if (s.equals("/")) {
+                finalList.add(value);
+                finalList.add("/");
+                value = "";
+            }
+        }
+        finalList.add(value);
+
+        for (String s : finalList) {
+            System.out.print(s + " ");
+        }
+    }
+}
---
+++ src/Calculations.java
@@ -0,0 +1,12 @@
+public class Calculations {
+
+
+}
+
+class Addition extends Calculations{
+
+}
+
+class subtraction extends Calculations{
+
+}
