2505dc48461685349bbb2dccc9e844f99b7807a6
danie <danieltoot2006@gmail.com>
2024-11-25 10:00:10 +0000

    added sin cos and tan

--- src/Calculations.java
+++ src/Calculations.java
@@ -1,68 +1,71 @@
 public class Calculations {
 
     static String add(String a, String b) {
-
         double number1 = Double.parseDouble(a);
         double number2 = Double.parseDouble(b);
-
         double c = number1 + number2;
         return String.valueOf(c);
     }
 
-
     static String subtract(String a, String b) {
-
         double number1 = Double.parseDouble(a);
         double number2 = Double.parseDouble(b);
-
         double c = number1 - number2;
         return String.valueOf(c);
     }
 
     static String multiply(String a, String b) {
-
         double number1 = Double.parseDouble(a);
         double number2 = Double.parseDouble(b);
-
         double c = number1 * number2;
         return String.valueOf(c);
     }
 
     static String divide(String a, String b) {
-
         double number1 = Double.parseDouble(a);
         double number2 = Double.parseDouble(b);
-
         double c = number1 / number2;
         return String.valueOf(c);
     }
 
     static String power(String a, String b) {
-
         double number1 = Double.parseDouble(a);
         double number2 = Double.parseDouble(b);
-
         number1 = Math.pow(number1, number2);
-
         return String.valueOf(number1);
     }
 
-    static String factorial(String a){
+    static String factorial(String a) {
         double number1 = Double.parseDouble(a);
         double result = 1;
 
-        while(number1 != 0){
-            if(Math.signum(number1) == 1.00){
+        while (number1 != 0) {
+            if (Math.signum(number1) == 1.00) {
                 result *= number1;
                 number1 -= 1;
-            }
-            else if(Math.signum(number1) == -1.00){
+            } else if (Math.signum(number1) == -1.00) {
                 result *= number1;
                 number1 += 1;
             }
         }
         return String.valueOf(result);
     }
+
+    // Sin operation
+    static String sin(String a) {
+        double number1 = Double.parseDouble(a);
+        return String.valueOf(Math.sin(Math.toRadians(number1))); // Convert degrees to radians
+    }
+
+    // Cos operation
+    static String cos(String a) {
+        double number1 = Double.parseDouble(a);
+        return String.valueOf(Math.cos(Math.toRadians(number1))); // Convert degrees to radians
+    }
+
+    // Tan operation
+    static String tan(String a) {
+        double number1 = Double.parseDouble(a);
+        return String.valueOf(Math.tan(Math.toRadians(number1))); // Convert degrees to radians
+    }
 }
-
-
