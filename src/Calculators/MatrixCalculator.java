287c12a4d2fd7eecdc58bd49dea51f9dd2ca4e76
AndyCao555 <andycao005@gmail.com>
2024-12-17 16:10:56 +0000

    Update MatrixCalculator.java

--- src/Calculators/MatrixCalculator.java
+++ src/Calculators/MatrixCalculator.java
@@ -4,15 +4,14 @@ import Menu.Calculators;
 
 import java.util.Scanner;
 
-public class MatrixCalculator extends Calculators{
+public class MatrixCalculator extends Calculators {
 
     Scanner scanner = new Scanner(System.in);
     int[][] matrix;
     int[][] matrix2;
     int matrixSize;
-    int fionn=0;
+    int fionn = 0;
     int[][] newMatrix;
-
 
 
     @Override
@@ -25,14 +24,14 @@ public class MatrixCalculator extends Calculators{
 
         for (int row = 0; row < matrixSize; row++) {
             for (int colum = 0; colum < matrixSize; colum++) {
-                System.out.println("Enter number for " +row+","+ colum);
+                System.out.println("Enter number for " + row + "," + colum);
                 matrix[row][colum] = scanner.nextInt();
             }
         }
 
         for (int row = 0; row < matrixSize; row++) {
-            for (int colum = 0; colum < matrixSize; colum++){
-                System.out.print("\t"+matrix[row][colum]);
+            for (int colum = 0; colum < matrixSize; colum++) {
+                System.out.print("\t" + matrix[row][colum]);
             }
             System.out.println();
 
@@ -40,15 +39,15 @@ public class MatrixCalculator extends Calculators{
 
         for (int row = 0; row < matrixSize; row++) {
             for (int colum = 0; colum < matrixSize; colum++) {
-                System.out.println("Enter number for " +row+","+ colum);
+                System.out.println("Enter number for " + row + "," + colum);
                 matrix2[row][colum] = scanner.nextInt();
             }
         }
 
 
         for (int row = 0; row < matrixSize; row++) {
-            for (int colum = 0; colum < matrixSize; colum++){
-                System.out.print("\t"+matrix2[row][colum]);
+            for (int colum = 0; colum < matrixSize; colum++) {
+                System.out.print("\t" + matrix2[row][colum]);
             }
             System.out.println();
 
@@ -63,6 +62,7 @@ public class MatrixCalculator extends Calculators{
                 add();
                 break;
             case "2":
+                subtract();
                 System.out.println();
                 break;
             case "3":
@@ -80,47 +80,69 @@ public class MatrixCalculator extends Calculators{
 
     }
 
-    public void square(){
+    public void square() {
         int[][] newMatrix = new int[matrixSize][matrixSize];
 
         for (int row = 0; row < matrixSize; row++) {
             for (int colum = 0; colum < matrixSize; colum++) {
-                for (int counter = 0;counter<matrixSize;counter++) {
+                for (int counter = 0; counter < matrixSize; counter++) {
 
-                    int b = matrix[counter][colum]*matrix2[row][counter];
+                    int b = matrix[counter][colum] * matrix2[row][counter];
                     fionn += b;
                 }
-                newMatrix[row][colum]= fionn;
-                fionn=0;
+                newMatrix[row][colum] = fionn;
+                fionn = 0;
             }
         }
 
         System.out.println("product of matrix is:");
         for (int row = 0; row < matrixSize; row++) {
-            for (int colum = 0; colum < matrixSize; colum++){
-                System.out.print("\t"+newMatrix[row][colum]);
+            for (int colum = 0; colum < matrixSize; colum++) {
+                System.out.print("\t" + newMatrix[row][colum]);
             }
             System.out.println();
         }
         System.out.println();
     }
 
-
-
-
-    public void add(){
-        int[][] newMatrix = new int[matrixSize][matrixSize];
+    public void add() {
+        int[][] newmatrix = new int[matrixSize][matrixSize];
 
         for (int row = 0; row < matrixSize; row++) {
             for (int colum = 0; colum < matrixSize; colum++) {
 
 
-                    int b = matrix[row][colum]+matrix2[row][colum];
-                newMatrix[row][colum]= b;
-                System.out.println(b);
+                int b = matrix[row][colum] + matrix2[row][colum];
+                newmatrix[row][colum] = b;
             }
+        }
+        System.out.println("Sum of matrices is:");
+        for (int row = 0; row < matrixSize; row++) {
+            for (int colum = 0; colum < matrixSize; colum++) {
+                System.out.print("\t" + newmatrix[row][colum]);
+            }
+            System.out.println();
+        }
+    }
+
+    public void subtract() {
+        int[][] newmatrix = new int[matrixSize][matrixSize];
+
+        for (int row = 0; row < matrixSize; row++) {
+            for (int colum = 0; colum < matrixSize; colum++) {
+                int b = matrix[row][colum] - matrix2[row][colum];
+                newmatrix[row][colum] = b;
+            }
+        }
+
+        System.out.println("Difference of matrices is:");
+        for (int row = 0; row < matrixSize; row++) {
+            for (int colum = 0; colum < matrixSize; colum++) {
+                System.out.print("\t" + newmatrix[row][colum]);
+            }
+            System.out.println();
+
+
         }
     }
 }
-
-
