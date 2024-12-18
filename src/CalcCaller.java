eeaeccdcb4d80328a79d68dfa00b4bf1531dc609
AndyCao555 <andycao005@gmail.com>
2024-12-18 02:14:07 +0000

    Update CalcCaller.java
    comments

--- src/CalcCaller.java
+++ src/CalcCaller.java
@@ -1,3 +1,4 @@
+//import statements for importing Calculator classes and menu funtionality
 import Calculators.ArithmeticCalculator;
 import Calculators.ArithmeticCalculatorInput;
 import Calculators.ProgrammingCalculator;
@@ -9,12 +10,12 @@ import Menu.Settings;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-
+//class responsible for handling Calculations 
 public class CalcCaller {
     public void calcCaller(){
         Scanner input = new Scanner(System.in);
         String ans = "";
-
+//initialize menu and calculator objects
         MenuItems calculators = new Calculators();
 
         ArithmeticCalculator arithmeticCalculator = new ArithmeticCalculator();
@@ -23,9 +24,9 @@ public class CalcCaller {
         Calculators matrixCalculator = new MatrixCalculator();
         Calculators calculatorChoice = new Calculators();
         Settings settings = new Settings();
-
+        //prompts for users calculator choice
         String calcChosen = calculatorChoice.calculatorChoice();
-
+        //switch statement used to select choice
         switch (calcChosen) {
             case "1":
                 ArrayList<String> finalList = ArithmeticCalculatorInput.input(ans);
@@ -42,10 +43,10 @@ public class CalcCaller {
             case "4":
                 matrixCalculator.runCalculator();
                 break;
-            default:
+            default://handeles invalid inputs
                 System.err.println("Invalid input");
-                calculators.display();
-                calcCaller();
+                calculators.display();//redisplays calculator menu
+                calcCaller();//restarts selection process
                 break;
         }
     }
