00cc5eb4408da4c8bd8e53e1b634fbd2dc1ec120
AndyCao555 <andycao005@gmail.com>
2024-12-18 02:24:56 +0000

    Update Calculators.java

--- src/Menu/Calculators.java
+++ src/Menu/Calculators.java
@@ -4,17 +4,18 @@ import java.util.Scanner;
 
 public class Calculators extends MenuItems {
     @Override
+    //method to display calculator options
     public void display() {
-        System.out.println("--------------------");
+        System.out.println("--------------------");//seperator
         System.out.println("1 - Arithmetic Calculator\n2 - Programming Calculator\n3 - Unit Converter\n4 - Matrix Calculator");
-        System.out.println("--------------------");
+        System.out.println("--------------------");//seperator
     }
-
+//method to get users choice 
     public String calculatorChoice() {
         Scanner input = new Scanner(System.in);
-        return input.nextLine();
+        return input.nextLine();//returns users input as string
     }
-
+//place holder method to be implemented for running a calculator,left empty too allow overriding from specific calculators
     public void runCalculator(){
 
     }
